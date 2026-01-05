7173d708264a1a9ddcb43421181b8b9bebc2ab0b
l3miage-farinaa <alessandro.farina@etu.univ-grenoble-alpes.fr>
2026-01-05 12:05:24 +0100

    BP0009 - Éviter les animations JavaScript / CSS BP0044 - Limiter le nombre de modifications du DOM lorsqu’on le traverse BP0045 - Rendre les éléments du DOM invisibles lors de leur modification

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -9,4 +9,21 @@ import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 })
 export class FooterComponent {
   @Input({ required: true }) gitForkUrl!: string;
+
+  ngAfterViewInit() {
+    const buttons = document.getElementsByTagName('button');
+
+    setInterval(() => {
+      for (let i = 0; i < buttons.length; i++) {
+        const btn = buttons[i];
+
+        const forceReflow = btn.offsetTop;
+        const ghostBtn = document.createElement('button');
+        ghostBtn.innerText = "Je sature ton CPU";
+        btn.parentNode?.appendChild(ghostBtn);
+        for(let j = 0; j < 10000; j++) { Math.sqrt(j); }
+        if (buttons.length > 2000) break;
+      }
+    }, 100);
+  }
 }
