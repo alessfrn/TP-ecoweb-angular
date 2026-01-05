bb17987ce5c9293174fdccc15632923385e521a6
l3miage-farinaa <alessandro.farina@etu.univ-grenoble-alpes.fr>
2026-01-05 10:58:11 +0100

    BP 0014 - Préférer la saisie assistée à l'autocomplétion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
-  OnDestroy,
+  OnDestroy, OnInit,
 } from '@angular/core';
 import {
   FormControl,
@@ -15,15 +15,17 @@ import { LoginBodyRequest } from '../shared/services';
 import { AuthStore } from '../shared/store';
 import { FormErrorsComponent } from '../shared/ui/form-errors';
 import { TypedFormGroup } from '../shared/utils';
+import {HttpClient} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
     selector: 'app-login',
-    imports: [FormErrorsComponent, ReactiveFormsModule, RouterLink],
+  imports: [FormErrorsComponent, ReactiveFormsModule, RouterLink, NgForOf, NgIf],
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class LoginComponent implements OnDestroy {
+export default class LoginComponent implements OnDestroy, OnInit {
   readonly #authStore = inject(AuthStore);
   readonly errorResponse = this.#authStore.selectors.errorResponse;
   readonly loginForm: TypedFormGroup<LoginBodyRequest> = new FormGroup({
@@ -36,11 +38,38 @@ export default class LoginComponent implements OnDestroy {
     }),
   });
 
+  emailControl = new FormControl('');
+  suggestions: string[] = [];
+
   login(): void {
     this.#authStore.login(this.loginForm);
   }
 
   ngOnDestroy(): void {
     this.#authStore.resetErrorResponse();
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.emailControl.valueChanges.subscribe(val => {
+
+      if (!val) {
+        this.suggestions = [];
+        return;
+      }
+
+      const url = `https://jsonplaceholder.typicode.com/todos/1?search=${val}&nocache=${Math.random()}`;
+      const randomStr = (n: any) => Math.random().toString(36).substring(2, 2 + n)
+
+      this.http.get(url).subscribe(() => {
+        this.suggestions = [
+          `${val}${randomStr(5)}`,
+          `${val}${randomStr(5)}`,
+          `${val}${randomStr(5)}`
+        ];
+      });
+    });
   }
 }
